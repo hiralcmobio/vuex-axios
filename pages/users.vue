4edb6658293bf3ce939d93158e0f5bb11ce981de
hiralcmobio <hiralc.mobio@gmail.com>
2020-03-27 19:18:47 +0530

    vuex-axios example

---
+++ pages/users.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Users</h1>
+    <div v-for="user in users" :key="user.id">
+      {{ user.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        users: [
+          {
+            id: '1',
+            name: 'Nitya'
+          },
+          {
+            id: '2',
+            name: 'Arav'
+          },
+          {
+            id: '3',
+            name: 'Heli'
+          },
+          {
+            id: '4',
+            name: 'Het'
+          }
+        ]
+      }
+    }
+
+  }
+</script>
+
+<style lang="scss" scoped></style>
